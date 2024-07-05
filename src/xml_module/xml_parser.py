b2683fd71f45aa47e64e10aab68f2d20554a3f85
Michail Mpastakis <michail.mpastakis@softcom-int.com>
2024-07-05 16:34:25 +0000

    feat: WIP creating extract_data functionality

--- src/xml_module/xml_parser.py
+++ src/xml_module/xml_parser.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 
 class XMLParser:
     
-    def __init__(self, xml_file_path=None) -> None:
+    def __init__(self, xml_file_path : str = None) -> None:
         self.xml_file_path = xml_file_path
         self.xml = None
     
@@ -15,4 +15,29 @@ class XMLParser:
             return self.xml
         except FileNotFoundError as e:
             raise e
+    
+    def extract_data(self, config : dict ) -> dict:
+        if self.xml is None:
+            raise FileNotFoundError('XML file is not parsed')
         
+        root_element = config.get('ROOT_ELEMENT', None)
+        if root_element is None:
+            raise ValueError('ROOT_ELEMENT is not set in config')
+        
+        product_element = config.get('PRODUCT_ROOT_ELEMENT', None)
+        if product_element is None:
+            raise ValueError('PRODUCT_ROOT_ELEMENT is not set in config')
+        
+        data = {}
+        
+        root = self.xml.getroot()
+        products_root = root.find(root_element)
+        
+        for product in products_root.findall(product_element):
+            data[product.get('id')] = "test"
+        
+        return data
+            
+        
+        
+    
--- tests/xml/test_xml.py
+++ tests/xml/test_xml.py
@@ -1,4 +1,12 @@
+import pytest
 from xml_module.xml_parser import XMLParser
+
+@pytest.fixture
+def expected_config():
+    return {
+        "ROOT_ELEMENT": "products",
+        "PRODUCT_ROOT_ELEMENT": "product",
+    }
 
 def test_create_xml_parser():
     xml_parser = XMLParser()
@@ -19,7 +27,7 @@ def test_parse_xml_throw_FileNotFoundException():
         assert xml_parser.xml is None
         assert True
 
-def test_parse_xml_correct_parse():
+def test_xml_parser_parse_xml():
     xml_parser = XMLParser('tests/xml/test_data/pakoworld.xml')
     try:
         xml = xml_parser.parse_xml()
@@ -28,3 +36,31 @@ def test_parse_xml_correct_parse():
         assert xml is xml_parser.xml
     except FileNotFoundError as e:
         assert False
+    
+def test_xml_parser_extract_data_easy( expected_config ):
+    xml_parser = XMLParser('tests/xml/test_data/easy_test.xml')
+    xml_parser.parse_xml()
+    
+    data = xml_parser.extract_data(expected_config)
+    
+    assert data is not None
+    assert len(data.keys()) == 1
+    
+def test_xml_parser_extract_data_pakoworld( expected_config ):
+    xml_parser = XMLParser('tests/xml/test_data/pakoworld.xml')
+    xml_parser.parse_xml()
+    
+    data = xml_parser.extract_data(expected_config)
+    
+    assert data is not None
+    assert len(data.keys()) == 42
+    
+
+def test_xml_parser_extract_data_with_empty_xml( expected_config ):
+    xml_parser = XMLParser('tests/xml/test_data/empty.xml')
+    xml_parser.parse_xml()
+    
+    data = xml_parser.extract_data(expected_config)     
+    
+    assert data is not None
+    assert len(data.keys()) == 0
